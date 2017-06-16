a8fd61a0348a8dd843c9dc09e44ef05b70aaa9ed
joshuakelley <joshkelley19@gmail.com>
2017-06-16 08:40:10 -0400

    began password controller

---
+++ src/main/java/kelley/josh/controller/PasswordController.java
@@ -0,0 +1,11 @@
+package kelley.josh.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by joshuakelley on 6/16/17.
+ */
+@RestController("/randomizer")
+public class PasswordController {
+//    todo make api call to password randomizer via service
+}
