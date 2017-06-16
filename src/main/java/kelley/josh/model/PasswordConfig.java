ae37daa4a3bf743337343b6381bba59b4c3e32a4
joshuakelley <joshkelley19@gmail.com>
2017-06-16 08:24:11 -0400

    password config model

---
+++ src/main/java/kelley/josh/model/PasswordConfig.java
@@ -0,0 +1,15 @@
+package kelley.josh.model;
+
+/**
+ * Created by joshuakelley on 6/16/17.
+ */
+public class PasswordConfig {
+//    todo overload method differentiating between fully random and specifics added
+    boolean custom;
+    int length;
+    int upperCase;
+    int lowerCase;
+    int numbers;
+    int characters;
+    boolean charStandard;
+}
